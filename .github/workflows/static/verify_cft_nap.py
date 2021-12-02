8f635f923d21e490afdfb5ac65a2586e31b5c26b
sshajiya <39581520+sshajiya@users.noreply.github.com>
2021-12-02 07:02:14 +0530

    Update verify_cft_nap.py

--- .github/workflows/static/verify_cft_nap.py
+++ .github/workflows/static/verify_cft_nap.py
@@ -1,7 +1,8 @@
 import boto3
 import os
-from utils import *
 import sys
+import re
+import requests
 
 nginx_default_page="Welcome to nginx"
 nginx_static_page="Hello World"
@@ -23,3 +24,25 @@ try:
     print("Error: NGINX APP PROTECT ", sys.argv[1].upper() , " PAGE VERIFICATION IS Failed...")
 except Exception as e:
   print("Exception raised with error:",e)
+
+def vfy_cft_link(cft_name,exp_op):
+  cf_client = boto3.client('cloudformation')
+  response = cf_client.describe_stacks(StackName=cft_name)
+  if not response["Stacks"][0]["Outputs"]:
+      print("no results in output section")
+      return False
+  outputs = response["Stacks"][0]["Outputs"]
+  print("output infor for the stack: ", outputs)
+  try:
+    for output in outputs:
+      if output["OutputKey"] == "AppProtectLBDNSName":
+        url="http://"+output["OutputValue"]
+        chk_data = requests.get(url)
+        if exp_op in chk_data.text:
+          print(chk_data.text)
+          return True
+        else:
+          print(chk_data.text)
+          return False
+  except Exception as e:
+    print("Exception raised with error:",e)
