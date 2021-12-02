e2c8a86ddcc2f9b89c2a0a3f7d9aa640e94e02db
sshajiya <39581520+sshajiya@users.noreply.github.com>
2021-12-02 08:00:59 +0530

    Update verify_cft_nap.py

--- .github/workflows/static/verify_cft_nap.py
+++ .github/workflows/static/verify_cft_nap.py
@@ -6,32 +6,31 @@ import requests
 
 def vfy_cft_link(cft_name,exp_op):
   '''
-	  This method is to verify the cloud formation output link status.
-    |  *cft_name*         | Name of the Stack                      |
-    |  *exp_op*           | expected output for the output link    |
-    |  *returns*          | True if outoput exists if not False    |
-	'''
-    cf_client = boto3.client('cloudformation')
-    response = cf_client.describe_stacks(StackName=cft_name)
-    if not response["Stacks"][0]["Outputs"]:
-        print("no results in output section")
-        return False
-    outputs = response["Stacks"][0]["Outputs"]
-    print("output infor for the stack: ", outputs)
-    try:
-      for output in outputs:
-        if output["OutputKey"] == "AppProtectLBDNSName":
-          url="http://"+output["OutputValue"]
-          chk_data = requests.get(url)
-          if exp_op in chk_data.text:
-            print(chk_data.text)
-            return True
-          else:
-            print(chk_data.text)
-            return False
-    except Exception as e:
-      print("Exception raised with error:",e)
-
+  This method is to verify the cloud formation output link status.
+  |  *cft_name*         | Name of the Stack                      |
+  |  *exp_op*           | expected output for the output link    |
+  |  *returns*          | True if outoput exists if not False    |
+  '''
+  cf_client = boto3.client('cloudformation')
+  response = cf_client.describe_stacks(StackName=cft_name)
+  if not response["Stacks"][0]["Outputs"]:
+    print("no results in output section")
+      return False
+  outputs = response["Stacks"][0]["Outputs"]
+  print("output infor for the stack: ", outputs)
+  try:
+    for output in outputs:
+      if output["OutputKey"] == "AppProtectLBDNSName":
+        url="http://"+output["OutputValue"]
+        chk_data = requests.get(url)
+        if exp_op in chk_data.text:
+          print(chk_data.text)
+          return True
+        else:
+          print(chk_data.text)
+          return False
+  except Exception as e:
+    print("Exception raised with error:",e)
 
 if __name__ == '__main__':
 
