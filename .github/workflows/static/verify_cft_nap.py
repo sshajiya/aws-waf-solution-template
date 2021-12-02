04c841c794572f40e564ea4e0c9d648218ec4e06
sshajiya <39581520+sshajiya@users.noreply.github.com>
2021-12-02 14:21:06 +0530

    Update verify_cft_nap.py

--- .github/workflows/static/verify_cft_nap.py
+++ .github/workflows/static/verify_cft_nap.py
@@ -1,16 +1,15 @@
 import boto3
-import os
 import sys
-import re
 import requests
 
+
 def vfy_cft_link(cft_name,exp_op):
-  '''
+  """
   This method is to verify the cloud formation output link status.
   |  *cft_name*         | Name of the Stack                      |
   |  *exp_op*           | expected output for the output link    |
   |  *returns*          | True if outoput exists if not False    |
-  '''
+  """
   cf_client = boto3.client('cloudformation')
   response = cf_client.describe_stacks(StackName=cft_name)
   if not response["Stacks"][0]["Outputs"]:
@@ -25,33 +24,28 @@ def vfy_cft_link(cft_name,exp_op):
         url="http://"+output["OutputValue"]
         chk_data = requests.get(url)
         if exp_op in chk_data.text:
-          print("Template Body:\n",chk_data.text)
           return True
         else:
-          print(chk_data.text)
           return False
   except Exception as e:
     print("Exception raised with error:",e)
 
+    
 if __name__ == '__main__':
-
-  nginx_default_page="Welcome to nginx"
-  nginx_static_page="Hello World"
-  nginx_dynamic_page="Arcadia Finance"
+  
   stack_name= "nap-cft-stack"
   page_type=sys.argv[1] 
   if page_type == "static":
-    page_type = nginx_static_page
+    page = "Hello World"
   elif page_type == "dynamic":
-    page_type = nginx_dynamic_page
+    page = "Arcadia Finance"
   else:
-    page_type = nginx_default_page
-  #Verifying functionality of NAP
+    page = "Welcome to nginx"
   try:
-    vfy_status=vfy_cft_link(stack_name,page_type)
+    vfy_status=vfy_cft_link(stack_name,page)
     if vfy_status:
-      print("Status: NGINX APP PROTECT ", sys.argv[1].upper() , "PAGE VERIFICATION IS COMPLETED SUCESSFULLY...")
+      print("Status: NGINX APP PROTECT ", page_type.upper() , "PAGE VERIFICATION IS COMPLETED SUCESSFULLY...")
     else:
-      print("Error: NGINX APP PROTECT ", sys.argv[1].upper() , " PAGE VERIFICATION IS Failed...")
+      print("Error: NGINX APP PROTECT ", page_type.upper() , " PAGE VERIFICATION IS Failed...")
   except Exception as e:
     print("Exception raised with error:",e)
